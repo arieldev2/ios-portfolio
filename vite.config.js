ae0358c59346170e3ab2505879044bd9c0dade39
Ariel Ortiz <arielortiz.dev@gmail.com>
2025-05-12 01:08:29 -0700

    base  updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ios-portfolio'
+  base: 'https://arieldev2.github.io/ios-portfolio/'
 })
